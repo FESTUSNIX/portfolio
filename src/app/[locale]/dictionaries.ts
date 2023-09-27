a4d70fbb6898a466b5c106f3d1337bbcf9d202c9
Festus <matkowsky808@gmail.com>
2023-09-27 17:29:53 +0200

    chore: Change dictionaries location

--- src/app/[locale]/dictionaries.ts
+++ src/app/[locale]/dictionaries.ts
@@ -2,8 +2,8 @@ import 'server-only'
 import { Locales } from '../../i18nConfig'
 
 const dictionaries = {
-	en: () => import('../dictionaries/en.json').then(module => module.default),
-	pl: () => import('../dictionaries/pl.json').then(module => module.default)
+	en: () => import('@/dictionaries/en.json').then(module => module.default),
+	pl: () => import('@/dictionaries/pl.json').then(module => module.default)
 }
 
 export const getDictionary = async (locale: Locales) => dictionaries[locale]()
