febbfb130a5be3021c8c8e65b0de21b6ce0b7d12
Festus <matkowsky808@gmail.com>
2023-12-27 21:51:34 +0100

    chore: Optimize images

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,8 +1,8 @@
 import { StaticImageData } from 'next/image'
 import BillboardsIcon from '/public/images/projects/billboards/icon.svg'
-import BillboardsPreview from '/public/images/projects/billboards/preview.png'
+import BillboardsPreview from '/public/images/projects/billboards/preview.webp'
 import HelenasSailingIcon from '/public/images/projects/helenassailing/icon.svg'
-import HelenasSailingPreview from '/public/images/projects/helenassailing/preview.png'
+import HelenasSailingPreview from '/public/images/projects/helenassailing/preview.webp'
 
 type Project = {
 	href: string
