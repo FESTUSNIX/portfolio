f2caf186de9e22fc2f97fd1265b6e803b124c6ad
Festus <matkowsky808@gmail.com>
2024-04-26 19:14:47 +0200

    chore: Add new project - h2o

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,6 +1,8 @@
 import { StaticImageData } from 'next/image'
 import BillboardsIcon from '/public/images/projects/billboards/icon.svg'
 import BillboardsPreview from '/public/images/projects/billboards/preview.webp'
+import H2OIcon from '/public/images/projects/h2o/icon.svg'
+import H2OPreview from '/public/images/projects/h2o/preview.webp'
 import HelenasSailingIcon from '/public/images/projects/helenassailing/icon.svg'
 import HelenasSailingPreview from '/public/images/projects/helenassailing/preview.webp'
 
@@ -12,6 +14,12 @@ type Project = {
 }
 
 export const projects: Project[] = [
+	{
+		href: 'https://h2otrzebinia.pl/',
+		name: 'H2O Artykuły Wod-Kan, Gaz, C.O.',
+		logo: H2OIcon,
+		image: H2OPreview
+	},
 	{
 		href: 'https://helenassailing.com/',
 		name: "Helena's Sailing",
