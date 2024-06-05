8a11272b58ce0e6198d7eef69b7a3a659d8ce6f4
Festus <matkowsky808@gmail.com>
2024-06-05 23:52:51 +0200

    chore: Improve projects section; Add new project

--- src/app/[locale]/components/sections/Projects.tsx
+++ src/app/[locale]/components/sections/Projects.tsx
@@ -1,5 +1,6 @@
 import TypographyH2 from '@/components/ui/Typography/H2'
-import { projects } from '@/constants/projects'
+import { PROJECT_TAGS, projects } from '@/constants/PROJECTS'
+import { Locales } from '@/i18nConfig'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Dictionary } from '../../dictionaries'
@@ -10,6 +11,7 @@ type Props = {
 
 export const ProjectsSection = ({
 	dict: {
+		locale,
 		home: {
 			projects: { heading }
 		}
@@ -31,24 +33,28 @@ export const ProjectsSection = ({
 						key={`project-${i}`}
 						href={project.href}
 						target='_blank'
-						className='relative aspect-[4/3] overflow-hidden rounded-md last:odd:col-span-full'>
-						<div className='absolute inset-0 flex flex-col flex-wrap justify-between px-4 py-3 md:flex-row md:content-between md:items-start md:gap-4'>
-							<div className='w-max rounded-full border bg-background px-2 py-1 text-sm font-light uppercase leading-none text-foreground'>
+						className='group relative aspect-[4/3] overflow-hidden rounded-md last:odd:col-span-full'>
+						<div className='absolute inset-0 z-10 flex flex-col flex-wrap justify-between px-4 py-3 md:flex-row md:content-between md:items-start md:gap-4'>
+							<div className='w-max rounded-full border border-border bg-background/80 px-2 py-1 text-sm font-light uppercase leading-none text-foreground backdrop-blur-md'>
 								{project.name}
 							</div>
 
 							<ul className='flex flex-wrap-reverse items-center gap-1'>
 								{project.tags.map((tag, i) => (
 									<li key={`tag-${i}`}>
-										<span className='inline-block w-max rounded-full border border-foreground/20 bg-foreground/50 px-2 py-1 text-xs font-light uppercase !leading-none text-background backdrop-blur-md lg:text-sm'>
-											{tag}
+										<span className='inline-block w-max rounded-full border border-foreground/20 bg-background/40 px-2 py-1 text-xs font-light uppercase !leading-none text-foreground backdrop-blur-md lg:text-sm'>
+											{PROJECT_TAGS[tag][locale as Locales]}
 										</span>
 									</li>
 								))}
 							</ul>
 						</div>
 
-						<Image src={project.image} alt={`${project.name}`} className='size-full object-cover' />
+						<Image
+							src={project.image}
+							alt={`${project.name}`}
+							className='z-0 size-full rounded-md object-cover brightness-90 duration-200 ease-in group-hover:brightness-100 md:brightness-50'
+						/>
 					</Link>
 				))}
 			</ul>
--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,39 +1,65 @@
+import { type Locales } from '@/i18nConfig'
 import { StaticImageData } from 'next/image'
-import BillboardsIcon from '/public/images/projects/billboards/icon.svg'
 import BillboardsPreview from '/public/images/projects/billboards/preview.webp'
-import H2OIcon from '/public/images/projects/h2o/icon.svg'
 import H2OPreview from '/public/images/projects/h2o/preview.webp'
-import HelenasSailingIcon from '/public/images/projects/helenassailing/icon.svg'
 import HelenasSailingPreview from '/public/images/projects/helenassailing/preview.webp'
+import SDNPreview from '/public/images/projects/sdn/temp-preview.png'
 
 type Project = {
 	href: string
 	name: string
-	logo: any
 	image: StaticImageData
-	tags: string[]
+	tags: ProjectTagKeys[]
 }
 
 export const projects: Project[] = [
 	{
 		href: 'https://h2otrzebinia.pl/',
 		name: 'H2O Artykuły Wod-Kan, Gaz, C.O.',
-		logo: H2OIcon,
 		image: H2OPreview,
-		tags: ['ui/ux design', 'development', 'marketing']
+		tags: ['UI_UX_DESIGN', 'DEVELOPMENT', 'MARKETING']
 	},
 	{
 		href: 'https://helenassailing.com/',
 		name: "Helena's Sailing",
-		logo: HelenasSailingIcon,
 		image: HelenasSailingPreview,
-		tags: ['ui/ux design', 'development', 'reservation system']
+		tags: ['UI_UX_DESIGN', 'DEVELOPMENT', 'BRAND_ASSETS']
 	},
 	{
 		href: 'https://reklamy.andrzejgotfryd.pl/',
 		name: 'Andrzej Gotfryd Powierzchnie Reklamowe',
-		logo: BillboardsIcon,
 		image: BillboardsPreview,
-		tags: ['ui/ux design', 'development']
+		tags: ['UI_UX_DESIGN', 'DEVELOPMENT', 'BRAND_ASSETS']
+	},
+	{
+		href: 'https://sdn-theta.vercel.app/',
+		name: 'Studia Dla Nauczycieli',
+		image: SDNPreview,
+		tags: ['UI_UX_DESIGN', 'DEVELOPMENT', 'STARTUP']
 	}
 ]
+
+type ProjectTagKeys = 'UI_UX_DESIGN' | 'DEVELOPMENT' | 'MARKETING' | 'BRAND_ASSETS' | 'STARTUP'
+
+export const PROJECT_TAGS: Record<ProjectTagKeys, Record<Locales, string>> = {
+	UI_UX_DESIGN: {
+		pl: 'Projekt UI/UX',
+		en: 'UI/UX Design'
+	},
+	DEVELOPMENT: {
+		pl: 'Stworzenie strony',
+		en: 'Development'
+	},
+	MARKETING: {
+		pl: 'Marketing',
+		en: 'Marketing'
+	},
+	BRAND_ASSETS: {
+		pl: 'Identyfikacja wizualna',
+		en: 'Brand Assets'
+	},
+	STARTUP: {
+		pl: 'Startup',
+		en: 'Startup'
+	}
+}
